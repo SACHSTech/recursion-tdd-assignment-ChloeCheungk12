ee8fef242aa638f99199e8e20fd6ec4bf9939081
ChloeCheungk12 <chloe.cheung21@ycdsbk12.ca>
2021-05-21 18:37:25 -0400

    Extreme Cases Green- Count7

--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -28,6 +28,24 @@ public class RecursionTest{
         assertEquals(5, Recursion.count7(71789777));
     }
 
+    @Test
+    public void Test4(){
+        // Extreme Case- all 7's
+        assertEquals(9, Recursion.count7(777777777));
+    }
+
+    @Test
+    public void Test5(){
+        // Extreme case- no 7's
+        assertEquals(0, Recursion.count7(1234568));
+    }
+
+    @Test
+    public void Test6(){
+        // Extreme Case- all 0's
+        assertEquals(0, Recursion.count7(0000000));
+    }
+
 
     
     
