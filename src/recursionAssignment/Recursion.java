e9b4e9d41d8a568aade5c6e75cea1655d154f379
ChloeCheungk12 <chloe.cheung21@ycdsbk12.ca>
2021-05-21 22:18:49 -0400

    Red- recursive case string clean

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -39,11 +39,12 @@ public class Recursion{
 
     public static String stringClean(String str){
         // base case
-        if (!str.equals("")) {
-            return "a";
+        if (str.length() < 2) {
+            return str;
         }
-        else {
-            return "";
+        // recursive step
+        else if (str.substring(0,1).equals(str.substring(1,2))) {
+            return str.substring(0,1) + stringClean(str.substring(2));
         }
 
         
